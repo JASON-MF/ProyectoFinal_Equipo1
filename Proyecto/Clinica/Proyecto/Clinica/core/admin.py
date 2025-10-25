79fb83256e8702364ab7b4a1341fe3181b7ab6b7
JASON-MF <mf0917032025@unab.edu.sv>
2025-10-25 16:49:13 -0600

    Delete Proyecto/Clinica/Proyecto directory

--- Proyecto/Clinica/Proyecto/Clinica/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Paciente, Medico, Cita, Especialidad, CentroMedico
-
-admin.site.register(Especialidad)
-admin.site.register(CentroMedico)
-admin.site.register(Paciente)
-admin.site.register(Medico)
-admin.site.register(Cita)
