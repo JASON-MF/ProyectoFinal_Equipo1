91bdc7407925dfb53cd0832ef254962856f809c2
Jason <mf0917032025@unab.edu.sv>
2025-11-02 21:22:11 -0600

    Subida inicial del proyecto final con estructura limpia

--- Proyecto/Proyecto/Clinica/Clinica/urls.py
+++
@@ -1,8 +0,0 @@
-# ...existing code...
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),  
-]
